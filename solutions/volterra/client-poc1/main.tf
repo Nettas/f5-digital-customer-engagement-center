985983dc70514cfb9865b1fbd210606d93f03705
Nettas <nettas@aol.com>
2021-10-20 08:03:36 -0400

    updated deployment

--- solutions/volterra/client-poc1/main.tf
+++ solutions/volterra/client-poc1/main.tf
@@ -16,8 +16,18 @@ provider "aws" {
   region = var.awsRegion
 }
 
+#provider "volterra" {
+#  timeout = "90s"
+#}
+
 provider "volterra" {
-  timeout = "90s"
+  timeout      = "90s"
+  api_p12_file = var.volterraP12
+  # url          = var.api_url
+  # api_p12_file = var.api_p12_file
+  # api_cert     = var.api_p12_file != "" ? "" : var.api_cert
+  # api_key      = var.api_p12_file != "" ? "" : var.api_key
+  url = var.volterraUrl
 }
 
 ########################### Locals ##########################
